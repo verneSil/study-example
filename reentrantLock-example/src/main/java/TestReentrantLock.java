0927f77d2f88c9094da8842a5788e1c23e02ef6d
左玮臣 <verne_ad@163.com>
2018-02-03 15:26:05 +0800

    1. fix future task

---
+++ reentrantLock-example/src/main/java/TestReentrantLock.java
@@ -0,0 +1,34 @@
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author verne on 18-2-3.
+ * @version 1.0
+ */
+
+public class TestReentrantLock {
+    public static void main(String[] args)  {
+        System.out.println("haha");
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        Condition write = lock.newCondition();
+        FutureTask futureTask = new FutureTask(() -> {
+            return "gg";
+        });
+        String o = new String();
+        futureTask.run();
+        System.out.println("isDone: " + futureTask.isDone());
+        try {
+            o = (String) futureTask.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println(o);
+        lock.unlock();
+    }
+}

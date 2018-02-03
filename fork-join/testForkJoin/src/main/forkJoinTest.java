ba6b8eaf7d8223611674c08983df67a37989458e
左玮臣 <verne_ad@163.com>
2018-02-03 11:12:57 +0800

    1. add fork join test

--- fork-join/testForkJoin/src/main/forkJoinTest.java
+++ fork-join/testForkJoin/src/main/forkJoinTest.java
@@ -1,7 +1,9 @@
 package main;
 
 import java.util.Arrays;
-import java.util.concurrent.RecursiveTask;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.*;
 
 /**
  * @author verne on 12/28/17.
@@ -26,33 +28,27 @@ public class forkJoinTest extends RecursiveTask<Boolean> {
     @Override
     protected Boolean compute() {
 
-        int mid = (0 + arr.length) / 2;
-
-        System.out.println(
-                Thread.currentThread().getName() + " says : After splliting the arry length is :: " + arr.length +
-                " Midpoint is " + mid);
-        if (arr[mid] == searchableElement) {
+        // edited by zuowc on 1/3/18 :
+        // not average fork/join
+        int mid = (0 + arr.length) / 2 - 3;
+        System.out.println(Thread.currentThread()
+                .getName() + " says : After splliting the arry length is :: " + arr.length + " Midpoint is " + mid);
+        if(arr[mid] == searchableElement) {
             System.out.println(" FOUND !!!!!!!!!");
             return true;
-        } else if (mid == 1 || mid == arr.length) {
+        } else if(mid == 1 || mid == arr.length) {
             System.out.println("NOT FOUND !!!!!!!!!");
             return false;
-        } else if (searchableElement < arr[mid]) {
+        } else if(searchableElement < arr[mid]) {
             System.out.println(Thread.currentThread().getName() + " says :: Doing Left-search");
-            int[] left = Arrays.copyOfRange(arr,
-                                            0,
-                                            mid);
-            forkJoinTest forkTask = new forkJoinTest(left,
-                                                     searchableElement);
+            int[] left = Arrays.copyOfRange(arr, 0, mid);
+            forkJoinTest forkTask = new forkJoinTest(left, searchableElement);
             forkTask.fork();
             return forkTask.join();
-        } else if (searchableElement > arr[mid]) {
+        } else if(searchableElement > arr[mid]) {
             System.out.println(Thread.currentThread().getName() + " says :: Doing Right-search");
-            int[] right = Arrays.copyOfRange(arr,
-                                             mid,
-                                             arr.length);
-            forkJoinTest forkTask = new forkJoinTest(right,
-                                                     searchableElement);
+            int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+            forkJoinTest forkTask = new forkJoinTest(right, searchableElement);
             forkTask.fork();
             return forkTask.join();
         }
